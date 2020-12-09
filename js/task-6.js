db6c64907815c36676b52ffb259fac4e7c453e1d
GolovinaElia <eliagolovina@ukr.net>
2020-12-09 23:38:33 +0200

    Добавляет файлы

--- js/task-6.js
+++ js/task-6.js
@@ -17,29 +17,22 @@
 //  while (input !== null);
 // console.log(`Общая сумма чисел равна ${total}`);  
 
-// let input;
-// let total = 0;
-
-// let inputNumbers = function (sum) {
-//         for (const number of numbers) {
-//         if (result.length > longestWord.length) {
-//             longestWord = result;
-//         }
-//     }
-//     return longestWord;
-// do {
-//     input = prompt('Введите число');
-//     if (isNaN(input)) {
-//         alert('Было введено не число, попробуйте еще раз');
-//     }
-//        else if (input === null) {
-//             const numbers = [];
-//             for (let i = 0; i < 5; i += 1) {
-//                 numbers.push(`${i}`);
-//             }
-//         } else {
-//             total += Number(input);
-//         }
-//     }
-//  while (input !== null);
-// console.log(`Общая сумма чисел равна ${total}`);  
+let input;
+let total = 0;
+do {
+    input = prompt('Введите число');
+    if (isNaN(input)) {
+        alert('Было введено не число, попробуйте еще раз');
+    }
+       else if (input === null) {
+            const numbers = [];
+            for (let i = 0; i < 2; i += 1) {
+                numbers[i]=i;
+                console.log(numbers);
+            }
+        } else {
+            total += Number(input);
+        }
+    }
+ while (input !== null);
+console.log(`Общая сумма чисел равна ${total}`);  
