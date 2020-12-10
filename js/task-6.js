0f716fad705ccba6b770bfc666331e133b6169e1
GolovinaElia <eliagolovina@ukr.net>
2020-12-10 23:29:48 +0200

    Добавляет массив

--- js/task-6.js
+++ js/task-6.js
@@ -1,38 +1,19 @@
-// let input;
-// let total = 0;
-// do {
-//     input = prompt('Введите число');
-//     if (isNaN(input)) {
-//         alert('Было введено не число, попробуйте еще раз');
-//     }
-//        else if (input === null) {
-//             const numbers = [];
-//             for (let i = 0; i < 5; i += 1) {
-//                 numbers.push(`${i}`);
-//             }
-//         } else {
-//             total += Number(input);
-//         }
-//     }
-//  while (input !== null);
-// console.log(`Общая сумма чисел равна ${total}`);  
-
 let input;
+const numbers = [];
 let total = 0;
-do {
+while (true) {
     input = prompt('Введите число');
-    if (isNaN(input)) {
+    if (input === null) {
+        break;
+    }
+    else if (isNaN(input)) {
         alert('Было введено не число, попробуйте еще раз');
+        continue;
+    } else {
+        for (let i = 0; i < numbers.lenght; i++);
+        numbers.push(input);
+        total += Number(input);
     }
-       else if (input === null) {
-            const numbers = [];
-            for (let i = 0; i < 2; i += 1) {
-                numbers[i]=i;
-                console.log(numbers);
-            }
-        } else {
-            total += Number(input);
-        }
-    }
- while (input !== null);
-console.log(`Общая сумма чисел равна ${total}`);  
+}
+   console.log(numbers);
+   console.log(`Общая сумма чисел равна ${total}`);
