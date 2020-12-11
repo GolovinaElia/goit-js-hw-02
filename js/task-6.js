40f86cbf8bad73e46f91383d6d8664f775613622
GolovinaElia <eliagolovina@ukr.net>
2020-12-11 22:02:06 +0200

    Добавляет файлы

--- js/task-6.js
+++ js/task-6.js
@@ -4,15 +4,16 @@ let total = 0;
 while (true) {
     input = prompt('Введите число');
     if (input === null) {
+        for (let i = 0; i < numbers.length; i += 1) {
+         total += Number(numbers[i]);   
+        }
         break;
     }
     else if (isNaN(input)) {
         alert('Было введено не число, попробуйте еще раз');
         continue;
     } else {
-        for (let i = 0; i < numbers.lenght; i++);
         numbers.push(input);
-        total += Number(input);
     }
 }
    console.log(numbers);
